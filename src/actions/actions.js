b886bbc4a0460b56afd4d2263927c04d02bf5fa3
joelpdurham <joeldur@gmail.com>
2020-02-20 21:28:19 -0800

    creates pizza action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,5 @@
 export const DRINK_COFFEE = 'DRINK_COFFEE';
+export const EAT_PIZZA = 'EAT_PIZZA';
 
-export const drinkCoffee = () => ({ type: DRINK_COFFEE })
+export const drinkCoffee = () => ({ type: DRINK_COFFEE });
+export const eatPizza = () => ({ type: EAT_PIZZA });
--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,7 +1,10 @@
-import { drinkCoffee } from './actions';
+import { drinkCoffee, eatPizza } from './actions';
 
 describe('ACTIONS TEST', () => {
-  it('create a drink coffee action', () => {
+  it('creates a drink coffee action', () => {
     expect(drinkCoffee().type).toEqual('DRINK_COFFEE');
+  });
+  it('creates a eat pizza action', () => {
+    expect(eatPizza().type).toEqual('EAT_PIZZA');
   });
 });
