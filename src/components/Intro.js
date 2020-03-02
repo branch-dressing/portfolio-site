52c91eac3247952c95411bc62ddf8dd26f6149cd
joelpdurham <joeldur@gmail.com>
2020-03-02 13:26:51 -0800

    reformatted intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,11 +8,11 @@ export const Intro = () => (
       <br/>
     I am a full stack developer, 
     </p>
-    <p className={styles.center}>theatre artist,
+    <p className={styles.center}><a href="https://www.wweek.com/arts/theater/2017/10/24/nesting-vacancy-is-part-haunted-house-part-twilight-zone-episode-and-shows-us-that-plays-can-be-really-really-scary/" rel="noopener noreferrer" target="_blank"><span className={styles.theatre}>theatre artist</span></a>,
       <br/>
-    book nerd, 
+      <a rel="noopener noreferrer" target="_blank" href="https://www.goodreads.com/user/show/60828906-joel"><span className={styles.books}>book nerd</span></a>,
       <br/>
-      and casual gamer,
+      and <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/eggcornco/?eid=ARBnTAa7pv4AEfnnTvDqhW3pGHB-hxB8nH0UeQhBkTnkc3O19IESn6wKfxvTDc2ovXIIQUp52cNgKgTt"><span className={styles.gamer}>casual gamer</span></a>,
     </p>
     <p className={styles.right}>
     living in Portland, OR.
